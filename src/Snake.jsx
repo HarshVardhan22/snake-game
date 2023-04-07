58c185c7b8477c1aeb7b6ead7195d23605c0c9de
Harshvardhan <hsonha31@gmail.com>
2023-04-07 19:51:36 +0530

    eating functionality added

--- src/Snake.jsx
+++ src/Snake.jsx
@@ -41,9 +41,11 @@ const Snake = () => {
   );
   const [gridSize, setGridSize] = useState(Math.sqrt(BLOCK_SIZE.length));
 
-  const [currentPositionOfSnake, setCurrentPositionOfSnake] = useState(() =>
-    changeTarget()
-  );
+  const [currentPositionOfSnake, setCurrentPositionOfSnake] = useState(() =>{
+    let arrayOfIndicesOfSnakeBody = [];
+   arrayOfIndicesOfSnakeBody.push(changeTarget())
+   return arrayOfIndicesOfSnakeBody
+  });
 
   const [snakeSize, setSnakeSize] = useState(1);
 
@@ -62,80 +64,98 @@ const Snake = () => {
   // this code is responsible to move the snake auotmatically in last selected direction
   useEffect(() => {
     let interval = setInterval(() => {
-      moveSnake(currentDirection);
+      moveSnake(currentDirection, true);
     }, [2000]);
     return () => clearInterval(interval);
   }, [currentDirection]);
 
-  const moveSnake = (keyCode) => {
+  const moveSnake = (keyCode, isMovingAutomatically = false) => {
     setCurrentDirection(keyCode);
     if (keyCode === "w") {
       //move up
-      checkSnakeBorderCollision(currentPositionRef.current - gridSize, "w");
+      checkSnakeBorderCollision(currentPositionRef.current.at(-1) - gridSize, "w", isMovingAutomatically );
     } else if (keyCode === "s") {
       //move down
-      checkSnakeBorderCollision(currentPositionRef.current + gridSize, "s");
+      checkSnakeBorderCollision(currentPositionRef.current.at(-1) + gridSize, "s", isMovingAutomatically);
     } else if (keyCode === "a") {
       //move left
-      checkSnakeBorderCollision(currentPositionRef.current - 1, "a");
+      checkSnakeBorderCollision(currentPositionRef.current.at(-1) - 1, "a", isMovingAutomatically);
     } else if (keyCode === "d") {
       //move right
-      checkSnakeBorderCollision(currentPositionRef.current + 1, "d");
+      checkSnakeBorderCollision(currentPositionRef.current.at(-1) + 1, "d", isMovingAutomatically);
     }
   };
 
-  const checkSnakeBorderCollision = (indexToBeChecked, movementDirection) => {
+  const checkSnakeBorderCollision = (indexToBeChecked, movementDirection, isMovingAutomatically) => {
     if (
-      currentPositionRef.current === borderIndices.cornerIndices[0] &&
+      currentPositionRef.current.at(-1) === borderIndices.cornerIndices[0] &&
       (movementDirection === "w" || movementDirection === "a")
     ) {
       setGameOver(true);
     } else if (
-      currentPositionRef.current === borderIndices.cornerIndices[1] &&
+      currentPositionRef.current.at(-1) === borderIndices.cornerIndices[1] &&
       (movementDirection === "w" || movementDirection === "d")
     ) {
       setGameOver(true);
     } else if (
-      currentPositionRef.current === borderIndices.cornerIndices[2] &&
+      currentPositionRef.current.at(-1) === borderIndices.cornerIndices[2] &&
       (movementDirection === "s" || movementDirection === "d")
     ) {
       setGameOver(true);
     } else if (
-      currentPositionRef.current === borderIndices.cornerIndices[3] &&
+      currentPositionRef.current.at(-1) === borderIndices.cornerIndices[3] &&
       (movementDirection === "s" || movementDirection === "a")
     ) {
       setGameOver(true);
     } else if (
-      borderIndices.topBorderIndices.indexOf(currentPositionRef.current) !==
+      borderIndices.topBorderIndices.indexOf(currentPositionRef.current.at(-1)) !==
         -1 &&
       movementDirection === "w"
     ) {
       setGameOver(true);
     } else if (
-      borderIndices.leftBorderIndices.indexOf(currentPositionRef.current) !==
+      borderIndices.leftBorderIndices.indexOf(currentPositionRef.current.at(-1)) !==
         -1 &&
       movementDirection === "a"
     ) {
       setGameOver(true);
     } else if (
-      borderIndices.rightBorderIndices.indexOf(currentPositionRef.current) !==
+      borderIndices.rightBorderIndices.indexOf(currentPositionRef.current.at(-1)) !==
         -1 &&
       movementDirection === "d"
     ) {
       setGameOver(true);
     } else if (
-      borderIndices.bottomBorderIndices.indexOf(currentPositionRef.current) !==
+      borderIndices.bottomBorderIndices.indexOf(currentPositionRef.current.at(-1)) !==
         -1 &&
       movementDirection === "s"
     ) {
       setGameOver(true);
-    } else setCurrentPositionOfSnake(indexToBeChecked);
+    } else {
+      if(targetLocation !== indexToBeChecked)
+        setCurrentPositionOfSnake((prev)=>{
+          let oldArrayOfPositions = [...prev]
+          console.log(oldArrayOfPositions)
+          let newArrayOfPositions = [];
+
+          for(let i = 1 ; i < oldArrayOfPositions.length; i++ ){
+            newArrayOfPositions.push(oldArrayOfPositions[i])
+          }
+          newArrayOfPositions.push(indexToBeChecked)
+          console.log(newArrayOfPositions)
+          return newArrayOfPositions
+        }) // since moving automatically no need to change the length
+      else if(targetLocation === indexToBeChecked){
+        setCurrentPositionOfSnake((prev)=>[...prev, indexToBeChecked]);
+      }
+        
+    }
   };
 
 
 
   useEffect(() => {
-    if(currentPositionOfSnake === targetLocation){
+    if(currentPositionOfSnake.at(-1) === targetLocation){
       setScore(prev=>prev+10);
       setSnakeSize(prev=>prev+1)
       setTargetLocation(()=>changeTarget());
@@ -161,7 +181,7 @@ const Snake = () => {
           <Block
             key={index}
             isThisTarget={index === targetLocation ? true : false}
-            isThisSnakeBody={index === currentPositionOfSnake ? true : false}
+            isThisSnakeBody={currentPositionOfSnake.indexOf(index) !== -1? true : false}
           >
             {item}
           </Block>
